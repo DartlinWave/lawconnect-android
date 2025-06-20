8b7751d3e702f435a942f19ec01ec0148254a65b
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-06-19 23:28:20 -0500

    refactor: update AppTopBar styling to use white background with shadow and display username only.

--- app/src/main/java/com/qu3dena/lawconnect/android/MainScreen.kt
+++ app/src/main/java/com/qu3dena/lawconnect/android/MainScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
@@ -21,6 +23,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.DefaultShadowColor
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -153,17 +159,25 @@ private fun RowScope.AddBottomBarItem(
 
 @Composable
 fun AppTopBar(username: String) {
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primary)
+            .shadow(
+                elevation = 10.dp
+            )
+            .background(Color.White)
             .padding(16.dp)
+            .padding(top = 20.dp)
     ) {
         Text(
-            text = "Hello, $username",
+            text = username,
             style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.onPrimary
+            color = Color.Black
         )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         Divider(
             color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
             thickness = 1.dp
